ca06a79ffb61f12a73f5fd31f041f6cd729d0211
brainmaze <devrin.hoefer@gmail.com>
2025-01-19 15:40:38 -0800

    yes

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ setTimeout(function() {
 showScore()
 }, 602000);
 
+const startingMinutes = 10;
+const countdownEl = document.getElementById('countdown');
+
+let time = startingMinutes * 60; //minutes * 60 seconds
+let refreshIntervalId = setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    const contdownEl = document.getElementById("f1"); 
+    countdownEl.innerHTML = `${minutes} : ${seconds}`;
+    time--;
+
+    if (time < 0) { //stop the setInterval whe time = 0 for avoid negative time
+        clearInterval(refreshIntervalId);
+        
+}
+};
+
+updateCountdown();
 
 
 
@@ -48,33 +70,15 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
-const startingMinutes = 10;
-const countdownEl = document.getElementById('countdown');
 
 let currentQuestionIndex = 0;
 let score = 0;
-let time = startingMinutes * 60; //minutes * 60 seconds
-let refreshIntervalId = setInterval(updateCountdown, 1000);
 
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
-}
-function updateCountdown(){
-
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    const contdownEl = document.getElementById("f1"); 
-    countdownEl.innerHTML = `${minutes} : ${seconds}`;
-    time--;
-
-    if (time < 0) { //stop the setInterval whe time = 0 for avoid negative time
-        clearInterval(refreshIntervalId);
-        
-}
 }
 
 function showQuestion(){
