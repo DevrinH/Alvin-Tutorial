ab9c43a56be8fdb6be9ac7ff5529702998b0fcaa
brainmaze <devrin.hoefer@gmail.com>
2025-01-19 17:30:56 -0800

    yes

--- script.js
+++ script.js
@@ -133,6 +133,7 @@ export function showScore(){
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
+    return true;
 }
 
 function handleNextButton(){
