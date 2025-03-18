d83d93aee4e72fc6833a45d6475f722698839192
brainmaze <devrin.hoefer@gmail.com>
2025-03-17 21:22:47 -0700

    yes

--- algebra-lessons.js
+++ algebra-lessons.js
@@ -274,8 +274,7 @@ function gradeQuiz() {
     console.log(`Quiz score: ${percentage}%`);
     
     localStorage.setItem("quizPercentage", percentage); // Store percentage in localStorage
-    window.location.href = "    });
-"; // Redirect to results page
+    window.location.href = "https://devrinh.github.io/Alvin-Tutorial/user-profile.html"; // Redirect to results page
 }
 
 function recordTestResults() {
